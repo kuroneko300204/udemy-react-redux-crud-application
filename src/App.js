9bef9b8df89c51c01276fe8f7c6c764bc8c50435
kuroneko300204 <chatnoir20140917@gmail.com>
2020-11-12 16:35:53 +0900

    12_もっとJSX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (<h1>Hello, world!</h1>);
+    // ***定数を使用したJSX***
+    // const greeting = "Hi, mikeneko!";
+    // const dom = <h1 className="foo">{greeting}</h1>;
+    // return dom;
+
+    // ***イベントを使用したJSX〜その1〜***
+    // return <input type="text" onClick={() => {console.log("I am clicked.")}} />
+
+    // ***イベントを使用したJSX〜その2〜***
+    // return <input type="text" onChange={() => {console.log("I am clicked.")}} />
+
+    // ***HTMLのforを使用したJSX***
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </React.Fragment>
+    )
   }
 }
 
