ba4e6b18388eaf919be591c74d2e5c64a50547c9
kuroneko300204 <chatnoir20140917@gmail.com>
2020-11-17 19:12:59 +0900

    16_props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
-  return <div>
-    <Cat />
-    <Cat />
-    <Cat />
-  </div>
+  // Nonameにageが設定されていないがデフォルト値がセットされる。
+  const profiles = [
+    { name:"Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Noname" }
+  ]
+
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
+  )
+  // return <div>
+  //   // 記述が冗長なので、リファクタリングする
+  //   <User name={"Taro"} age={10} />
+  //   <User name={"Hanako"} age={5} />
+  // </div>
 }
 
-const Cat = () => {
-  return <div>Meow</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old !</div>
+}
+
+// propsのデフォルト値を設定できる
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
