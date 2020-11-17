0c04da38859c6f770e0e57f3f2860268e4b328bb
kuroneko300204 <chatnoir20140917@gmail.com>
2020-11-17 18:33:30 +0900

    15_Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    // ***定数を使用したJSX***
-    // const greeting = "Hi, mikeneko!";
-    // const dom = <h1 className="foo">{greeting}</h1>;
-    // return dom;
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-    // ***イベントを使用したJSX〜その1〜***
-    // return <input type="text" onClick={() => {console.log("I am clicked.")}} />
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+}
 
-    // ***イベントを使用したJSX〜その2〜***
-    // return <input type="text" onChange={() => {console.log("I am clicked.")}} />
-
-    // ***HTMLのforを使用したJSX***
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked.")}} />
-      </React.Fragment>
-    )
-  }
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
