3dc2d787f4303b912672bc2d937317f6f89b52ed
kuroneko300204 <chatnoir20140917@gmail.com>
2021-11-16 17:08:32 +0900

    23.connectでstateとactionsの関連付けを行う

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement } from '../actions'
 
 // ボタンをクリックすると表示している値がカウントアップまたはダウンする機能
-class Counter extends Component {
-  // 初期化処理
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-  // pulusカウントする
-  handlePlusButton = () => {
-    console.log("handlePlusButton")
-    console.log(this.state.count)
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  // minusカウントする
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }  
+class App extends Component {
 
   render() {
+    const props = this.props
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={ props.increment }>+1</button>
+        <button onClick={ props.decrement }>-1</button>
       </React.Fragment>
 
     )
   }
 
-  // handlePlusButton = () => {
-  //   // [NG]下記の様に直接状態を変更するのはNG! 再レンダーしてくれないから。
-  //   // this.state = { count: this.state.count + 1 }
-  //   // [OK]setStateが実行されることで、renderメソッドを一緒に呼び出してくれる(自動で再レンダー)。
-  //   // 状態を変更するときはsetStateを使用すること！プログラマーが意識せずに済む。
-  //   this.setState({ count: this.state.count + 1 })
-  // }
-
-  // handleMinusButton = () => {
-  //   this.setState({ count: this.state.count - 1 })
-  // }
-
-  // render() {
-  //   console.log("render")
-  //   return (
-  //     <React.Fragment>
-  //       <div>count: { this.state.count }</div>
-  //       <button onClick={this.handlePlusButton}>+1</button>
-  //       <button onClick={this.handleMinusButton}>-1</button>
-  //     </React.Fragment>
-  //   )
-  // }
 }
+const mapStateToProps = state => ({ value: state.count.value })
+// const mapDispatchToProps = dispatch =>({
+//   increment: ()=> dispatch(increment()),
+//   decrement: ()=> dispatch(decrement()),
+// })
+// 下記のような簡易な記述もできる。
+const mapDispatchToProps = ({ increment, decrement })
 
-export default App;
+// ActionとReducerを関連づけるconnect
+export default connect(mapStateToProps, mapDispatchToProps)(App)
